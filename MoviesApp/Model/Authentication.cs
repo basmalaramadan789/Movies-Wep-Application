f6b5e1ea0ac6b200271e06c05c3ad4872273fa0f
basmalaramadan789 <160773845+basmalaramadan789@users.noreply.github.com>
2024-04-25 17:10:03 +0200

    project file

--- MoviesApp/Model/Authentication.cs
+++ MoviesApp/Model/Authentication.cs
@@ -5,7 +5,7 @@
         public string Message { get; set;}
         public bool IsAuthenticated { get; set;}
         public string UserName { get; set;}
-        public string Email { get; set;}
+        public string Email { get; set;} 
         public List<string> Roles { get; set;}
         public string token { get; set; }
         public DateTime ExpireOn { get; set; }
